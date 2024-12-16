ad700fdf1de4f21f68a1a307abe2990c0a85b734
Mariya Shusharina <mariya.shushar@gmail.com>
2024-12-16 20:26:58 +0300

    feat: do tasks 19-21

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -380,8 +380,8 @@ function getSine(n) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(num, b) {
+  return num.toString(b);
 }
 
 /**
@@ -394,8 +394,8 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(n, f) {
+  return parseFloat(n).toExponential(f);
 }
 
 /**
@@ -409,8 +409,8 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(n, f) {
+  return n.toFixed(f);
 }
 
 /**
