632791e8f33fd70c10c6b3212aa32798387e26c5
Mariya Shusharina <mariya.shushar@gmail.com>
2024-12-16 21:25:15 +0300

    feat: do tasks 22-25

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -425,8 +425,8 @@ function toFixed(number, fractionDigits) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**
@@ -439,8 +439,8 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(n) {
+  return n.valueOf();
 }
 
 /**
@@ -458,8 +458,17 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  let d = false;
+  d = false;
+  if (
+    typeof number === 'number' &&
+    Number.isFinite(number) &&
+    !Number.isNaN(number)
+  ) {
+    d = true;
+  }
+  return d;
 }
 
 /**
@@ -473,8 +482,8 @@ function isNumber(/* number */) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  return Number.isInteger(number);
 }
 
 /**
